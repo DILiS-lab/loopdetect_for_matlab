3cc8a367afa0afef33d2d9109375ce37a80d99af
Katharina Baum <katharina.baum@lih.lu>
2020-06-24 15:47:53 +0200

    update reference for example function func_POSm4

--- func_POSm4.m
+++ func_POSm4.m
@@ -1,7 +1,11 @@
 function[dx]=func_POSm4(t,x,klin,knonlin)
     % example function: chain with positive regulation [Baum et al., 2016]
+    % 
+    % Baum K, Politi AZ, Kofahl B, Steuer R, Wolf J. Feedback, Mass 
+    % Conservation and Reaction Kinetics Impact the Robustness of Cellular 
+    % Oscillations. PLoS Comput Biol. 2016;12(12):e1005298
+    
     dx=zeros(4,1);
-    %J=jacob_POSm4(t,x,klin,knonlin);
     dx(1)= klin(1)-(klin(2)*(1 + (x(4)/knonlin(1))^knonlin(2))+klin(3))*x(1);
     dx(2)= klin(2)*(1 + (x(4)/knonlin(1))^knonlin(2))*x(1) - (klin(4) + klin(5))*x(2);
     dx(3)= klin(4)*x(2) - (klin(6) + klin(7))*x(3);
